6dbbb605d62c0aee33be665e1930041f29137a17
gimyboya <b.afatah@yahoo.com>
2019-05-01 02:06:55 +0800

    feat(init): initial set up

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  options: {
+    customProperties: true
+  },
+  iconfont: 'mdi',
+})
