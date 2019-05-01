a9cdbad377494dddf1a6584f44bc2a5c34ca1a49
gimyboya <b.afatah@yahoo.com>
2019-05-01 23:47:45 +0800

    feat(init): setting up

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,75 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+  <v-app id="inspire" :dark="dark">
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>CYK Parser</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            flat
+            icon
+            v-on="on"
+            href="https://github.com/gimyboya/Emotwit/issues/new/choose"
+            target="_Blank_"
+          >
+            <v-icon>mdi-fire</v-icon>
+          </v-btn>
+        </template>
+        <span>Found a bug or got a hot idea?</span>
+      </v-tooltip>
+      <v-btn flat icon @click="dark = !dark">
+        <v-icon>mdi-compare</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-layout row wrap>
+          <v-flex px-1 mb-2 sx12 md6>
+            <tweet></tweet>
+          </v-flex>
+          <v-flex px-1 mb-2 sx12 md6></v-flex>
+          <v-flex sx12></v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
+    <v-footer app fixed>
+      <v-btn flat icon href="https://t.me/bafatah" target="_Blank_">
+        <v-icon>mdi-telegram</v-icon>
+      </v-btn>
+      <v-btn flat icon href="https://twitter.com/gimyboya" target="_Blank_">
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn flat icon href="https://github.com/gimyboya/Emotwit" target="_Blank_">
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Tweet from "./components/Tweet";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Tweet
   },
-  data () {
+  data() {
     return {
-      //
-    }
+      dark: true
+    };
   }
-}
+};
 </script>
+
+<style>
+.wrapper {
+  flex-grow: 0.3;
+}
+footer {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Tweet.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card height="100%">
+    <v-card-title>Grammar</v-card-title>
+    <v-card-text>
+      <v-text-field
+        outline
+        label="Keyword"
+        placeholder="Enter a hashtag or a keyword"
+        v-model="input"
+      ></v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="analyse">Analyse</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: ""
+    };
+  },
+  methods: {
+    analyse() {}
+  }
+};
+</script>
+
+<style>
+</style>
