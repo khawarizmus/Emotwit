87d6b5d19ee1672f114e44602ccef5f4c7680fe4
gimyboya <b.afatah@yahoo.com>
2019-05-09 03:49:06 +0800

    fix(store): Type definitions for the state

--- src/store.ts
+++ src/store.ts
@@ -4,13 +4,26 @@ import { format } from "date-fns";
 
 Vue.use(Vuex);
 
+interface ITweets {
+  [key: string]: any[];
+}
+
+interface IState {
+  tweets: ITweets;
+  until: string;
+  type: string;
+  lang: string;
+}
+
+const state: IState = {
+  tweets: {},
+  until: format(Date.now(), "YYYY-MM-DD"),
+  type: "mixed",
+  lang: "en",
+};
+
 export default new Vuex.Store({
-  state: {
-    tweets: {},
-    until: format(Date.now(), "YYYY-MM-DD"),
-    type: "mixed",
-    lang: "en",
-  },
+  state,
   mutations: {
     ['PUSH-TWET'](state, tweets) {
       // mutate state
